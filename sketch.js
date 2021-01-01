e782232bb05826e0ca8eb8f09cfc0b12fa62a96f
arnav-sharma-24th <71770733+arnav-sharma-24th@users.noreply.github.com>
2021-01-01 15:50:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,156 @@
+var PLAY=1
+var END=0;
+var gameState=PLAY
+var score
+var backround,back;
+var bird,birdImage;
+var dragon,dragonImage;
+var helicopterImage;
+var meteor,meteorImage;
+var restartImg,restart;
+var gameOverImg,gameOver;
+var helicopterGroup,meteorGroup,dragonGroup;
+
+function preload(){
+  birdImage=loadAnimation("bird.png","birdman.png");
+  dragonImage=loadImage("draco.png");
+  helicopterImage=loadImage("cancould.png");
+  meteorImage=loadImage("nuke.png");
+  back = loadImage("backround.png");
+   restartImg = loadImage("reset.png")
+  gameOverImg = loadImage("gameOver2.png");
+  obstaclesGroup = new Group();
+  helicopterGroup = new Group();
+  meteorGroup = new Group();
+  dragonGroup = new Group();
+}
+
+function setup() {
+ createCanvas(600,450);
+  
+  backround = createSprite(200,250,6000,2000);
+  backround.addImage("round",back)
+  backround.scale=5;
+  
+  bird=createSprite(100,100,10,10)
+  bird.addAnimation("fly",birdImage);
+
+  bird.scale=1.2;
+ 
+  bird.setCollider("rectangle",0,0,30,30);
+  bird.debug = true;
+
+
+   gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(300,200);
+  restart.addImage(restartImg);
+  
+  
+  score = 0;
+}
+
+function draw() {
+  
+ background("white");
+ camera.x = bird.x;
+ 
+  if(gameState===PLAY){
+
+gameOver.visible = false;
+restart.visible = false;
+restart.x=bird.x;
+gameOver.x=bird.x;
+bird.velocityX =2
+
+score = score + Math.round(getFrameRate()/60);
+ if (backround.x < 75){
+  backround.x = backround.width/2;
+}
+
+ if(keyDown("space")) {
+bird.velocityY = -5;
+ }
+
+if(bird.isTouching(helicopterGroup) ||bird.isTouching(meteorGroup)||bird.isTouching(dragonGroup)||bird.y>450||bird.y<0){
+   
+  gameState =END;
+   }
+
+bird.velocityY=bird.velocityY+0.18;
+ spawnMeteor();
+spawnHeli();
+spawnDragon();
+  }
+  
+  else if(gameState===END){
+backround.velocityX=0;
+bird.velocityY=0;
+helicopterGroup.setVelocityXEach(0);
+meteorGroup.setVelocityXEach(0);
+dragonGroup.setVelocityXEach(0);
+helicopterGroup.setLifetimeEach(-1);
+   meteorGroup.setLifetimeEach(-1);
+ dragonGroup.setLifetimeEach(-1);
+gameOver.visible = true;
+restart.visible = true;
+score = 0;
+bird.velocityX =0
+ if ( mousePressedOver(restart)) {
+  gameState = PLAY;
+   helicopterGroup.destroyEach();
+   meteorGroup.destroyEach();
+   dragonGroup.destroyEach();
+   bird.y=200;
+   bird.velocityX =0
+   bird.x=100
+  
+}
+
+  }
+  
+  
+  drawSprites();
+  textSize(23)
+  text("score"+score,bird.x,50);
+}
+
+function spawnMeteor(){
+ if (frameCount % 60 === 0){
+   var meteor = createSprite(bird.x+300,Math.round(random(100,400)),50,50)
+   meteor.addImage(meteorImage);
+   meteor.scale=0.12;
+   meteor.velocityX=-(6 + score/100);
+   meteor.setCollider("rectangle",0,0,30,30);
+   meteorGroup.add(meteor);
+  
+
+ }
+}
+
+
+function spawnHeli(){
+ if (frameCount % 50 === 0){
+   var helicopter = createSprite(bird.x+300,Math.round(random(100,400)),50,50)
+   helicopter.addImage(helicopterImage);
+  helicopter.scale=0.2;
+  helicopter.velocityX=-(6 + score/100);
+  helicopterGroup.add(helicopter); 
+  
+
+ }
+}
+
+function spawnDragon(){
+ if (frameCount % 120 === 0){
+   var dragon = createSprite(bird.x+300,Math.round(random(100,400)),50,50)
+   dragon.addImage(dragonImage);
+  dragon.scale=2;
+  dragon.setCollider("rectangle",0,0,30,30);
+  dragon.velocityX=-(6 + score/100);
+  dragonGroup.add(dragon); 
+  
+
+ }
+}
